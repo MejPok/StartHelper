24e548f73d676930338d1eb0a11a9e6d53c40051
lukasprogram <lukas.je@post.cz>
2025-12-30 23:01:09 +0100

    wokring except spotify

--- StartHelper/MainWindow.xaml.cs
+++ StartHelper/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,9 +23,82 @@ namespace StartHelper
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void StartOver(object sender, RoutedEventArgs e)
         {
+            StartApp("overwolf");
 
         }
+        private void StartHotkey(object sender, RoutedEventArgs e)
+        {
+            StartApp("hotkey");
+
+        }
+        private void StartVisual(object sender, RoutedEventArgs e)
+        {
+            StartApp("visual");
+        }
+
+        private void KillOver(object sender, RoutedEventArgs e)
+        {
+            StopApp("Overwolf");
+        }
+        private void KillRiot(object sender, RoutedEventArgs e)
+        {
+            StopApp("Riot Client");
+        }
+
+        private void PlayMusic(object sender, RoutedEventArgs e)
+        {
+            string spotifyUri = ConfigurationManager.AppSettings["SpotifyPlaylist"];
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = spotifyUri,
+                UseShellExecute = true
+            });
+        }
+        
+        private void OpenGithub(object sender, RoutedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = ConfigurationManager.AppSettings["Opera"],
+                Arguments = "https://github.com/MejPok",
+                UseShellExecute = true
+            });
+        }
+        
+        private void StartApp(string app)
+        {
+            if(app == "hotkey")
+            {
+                string hotkeyScriptPath = ConfigurationManager.AppSettings["HotkeyScriptPath"];
+                Process.Start(hotkeyScriptPath);
+            } else if(app == "visual")
+            {
+                string visualStudioPath = ConfigurationManager.AppSettings["VisualStudioPath"];
+                Process.Start(visualStudioPath);
+            } else if(app == "overwolf")
+            {
+                string overwolfPath = ConfigurationManager.AppSettings["Overwolf"];
+                Process.Start(overwolfPath);
+            }
+            
+        }
+
+        private void StopApp(string app)
+        {
+            try
+            {
+                foreach (var process in Process.GetProcessesByName(app))
+                {
+                    process.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+
     }
 }
